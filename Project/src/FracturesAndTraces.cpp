0e223907fcbbc1a4b29aacb80aa0b27d1c444b02
Alessia Ma <s297296@studenti.polito.it>
2024-05-11 11:56:21 +0200

    File rinominati 1

---
+++ Project/src/FracturesAndTraces.cpp
@@ -0,0 +1,11 @@
+#include <FracturesAndTraces.hpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <array>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using namespace Eigen;
---
+++ Project/src/FracturesAndTraces.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <Eigen/Eigen>
+#include <vector>
+#include <array>
+
+using namespace std;
+using namespace Eigen;
